cc71681efd6c16ee4f3d74b41f5b729d76cd4d0c
PamirAI <founders@pamir.ai>
2024-07-13 17:30:30 -0700

    updated fan speed config, added fan monitor in boot_up.sh

--- boot_up.sh
+++ boot_up.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# hijack screen over
 raspi-gpio set 22 pu
 sleep 0.01
 raspi-gpio set 22 pd
 sleep 0.01
+
+# adjust fan speed
+/home/distiller/DistillerSDK/src/distiller/firmware/Fan/daemon.sh &
 
 cd /home/distiller/DistillerSDK
 venv/bin/python examples/main.py 
